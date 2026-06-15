84e8e88ecf9029978ea57efeb7afb5ee1c23932e
tomato <taoxinyu_27@qq.com>
2026-06-15 23:57:42 +0800

    use tiny tool thumbnails and reduce loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,14 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useState } from "react";
 import CharacterSelectPage from "./pages/CharacterSelectPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import GamePage from "./pages/GamePage.jsx";
 import KnowledgeCollectionPage from "./pages/KnowledgeCollectionPage.jsx";
 import ResultPage from "./pages/ResultPage.jsx";
-import { characters } from "./data/characters.js";
-import { routes } from "./data/routes.js";
-import { preloadImages } from "./utils/preloadImages.js";
 
 export default function App() {
   const [screen, setScreen] = useState("home");
   const [routeId, setRouteId] = useState("nick");
   const [result, setResult] = useState(null);
-  const warmupList = useMemo(() => {
-    const routeBackgrounds = Object.values(routes).flatMap((route) =>
-      Object.values(route.nodes || {}).map((node) => node.background),
-    );
-    const characterImages = Object.values(characters).flatMap((character) =>
-      Object.values(character.images || {}),
-    );
-
-    return [...routeBackgrounds, ...characterImages];
-  }, []);
-
-  useEffect(() => {
-    const startWarmup = () => {
-      preloadImages(warmupList);
-    };
-
-    if ("requestIdleCallback" in window) {
-      const id = window.requestIdleCallback(startWarmup, { timeout: 2500 });
-      return () => window.cancelIdleCallback(id);
-    }
-
-    const id = window.setTimeout(startWarmup, 1200);
-    return () => window.clearTimeout(id);
-  }, [warmupList]);
 
   function openCharacterSelect() {
     setResult(null);
